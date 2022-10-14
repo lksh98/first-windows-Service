ce99e425a0b9499adf05a9316bf557751ec7c81d
lksh98 <kumarlokesh9798@gmail.com>
2022-10-14 11:06:37 +0530

    first commit

---
+++ Diskcleaner/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diskcleaner
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new Service1()
+            };
+            ServiceBase.Run(ServicesToRun);
+        }
+
+            //#if (!DEBUG)
+            //        ServiceBase[] ServicesToRun;
+            //            ServicesToRun = new ServiceBase[]
+            //            {
+            //                new Service1()
+            //            };
+            //            ServiceBase.Run(ServicesToRun);}
+
+            //#else
+            //            Service1 src = new Service1();
+            //            src.Deletfiles();
+            //#endif
+
+        }
+    }
+
---
+++ Diskcleaner/Service1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace Diskcleaner
+{
+    public partial class Service1 : ServiceBase
+    {
+        Timer Timer;
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            Timer = new Timer(2000)
+            {
+                AutoReset = true
+            };
+            Timer.Elapsed += new ElapsedEventHandler(timer_elapsed);
+            Timer.Start();
+        }
+        private void timer_elapsed(object sender,ElapsedEventArgs args)
+        {
+            writeLog();
+        }
+        protected override void OnStop()
+        {
+            Timer.Stop();
+            Timer = null;
+        }
+        public void Deletfiles()
+        {
+          Directory.GetFiles("D:tempfiles").Select(f => new FileInfo(f))
+         .Where(f => f.CreationTime < DateTime.Now.AddMonths(-3))
+         .ToList()
+         .ForEach(f => f.Delete());
+        }
+        private void writeLog()
+        {
+            //string path = @"c:\lokeshTesting\Logger" + System.DateTime.Now.ToShortTimeString() + ".txt";
+            //if (!File.Exists(path))
+            //{
+            //    using (StreamWriter sw = File.CreateText(path))
+            //    {
+            //        sw.WriteLine($"exe running on :{DateTime.Now}");
+            //    }
+            //}
+            StreamWriter writer = null;
+            writer = new StreamWriter(AppDomain.CurrentDomain.BaseDirectory + "\\log.txt",true);
+            writer.WriteLine($"exe running on :{DateTime.Now}");
+            writer.Flush();
+            writer.Close();
+        }
+    }
+}
